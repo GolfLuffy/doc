ed9f8251aa001303286a487e471351fea1994bd6
GolfLuffy <154314380+GolfLuffy@users.noreply.github.com>
2024-04-30 04:03:16 +0700

    Add files via upload

---
+++ trade/db_init_curated_trd_fin_view.sql
@@ -0,0 +1,19 @@
+-- Databricks notebook source
+/*---------------------------------------------------------------------------------------------------
+# Name: db_init_curated_trd_fin_view.sql
+# Area: trd_fin
+#----------------------------------------------------------------------------------------------------
+#
+# Change Revision
+#----------------------------------------------------------------------------------------------------
+# Date....           Who....           Description....
+#----------------------------------------------------------------------------------------------------
+# 2023-10-26         Jirat B.          Initial Version
+# 2024-02-15         Jirat B.          Add Curated View
+#
+# Target Database(s): ${catalog}.curated_trd_fin_view
+#--------------------------------------------------------------------------------------------------*/
+
+-- COMMAND ----------
+
+create database if not exists ${catalog}.curated_trd_fin_view managed location 'abfss://${storage_curated}/${catalog}/curated_trd_fin_view';
